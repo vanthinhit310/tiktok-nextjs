3b3765c41e54acc30b4c98bdd0dbf82aebdb0a68
Lê Văn Thịnh <vanthinhit310@gmail.com>
2021-10-22 00:12:22 +0700

    -

--- components/home/Sidebar.js
+++ components/home/Sidebar.js
@@ -1,7 +1,31 @@
+import { Box, List, ListItem, Icon } from "@chakra-ui/react";
+import { AiFillHome } from "react-icons/ai";
+import { FiUsers } from "react-icons/fi";
+import Link from "next/link";
+
 function Sidebar() {
     return (
         <section className="sidebar">
-            <p>Some great content...</p>
+            <div className="sidebar-content">
+                <Box w="100%">
+                    <List spacing={3}>
+                        <ListItem className="sidebar-item-hover">
+                            <Link href="/">
+                                <a>
+                                    <Icon as={AiFillHome} mr={2} boxSize="1.4rem" /> For You
+                                </a>
+                            </Link>
+                        </ListItem>
+                        <ListItem className="sidebar-item-hover">
+                            <Link href="/">
+                                <a>
+                                    <Icon as={FiUsers} mr={2} boxSize="1.4rem" /> Following
+                                </a>
+                            </Link>
+                        </ListItem>
+                    </List>
+                </Box>
+            </div>
         </section>
     );
 }
