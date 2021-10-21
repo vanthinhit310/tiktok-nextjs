db652318a52ebf519f13094279299e05fcbde0e8
Lê Văn Thịnh <vanthinhit310@gmail.com>
2021-10-22 02:42:33 +0700

    -

--- components/home/Sidebar.js
+++ components/home/Sidebar.js
@@ -1,29 +1,81 @@
-import { Box, List, ListItem, Icon } from "@chakra-ui/react";
+import { Box, List, ListItem, Icon, Text, Button, Link, Flex, Spacer, Avatar, HStack, Wrap, WrapItem, Tag } from "@chakra-ui/react";
 import { AiFillHome } from "react-icons/ai";
 import { FiUsers } from "react-icons/fi";
-import Link from "next/link";
+import NextLink from "next/link";
 
 function Sidebar() {
     return (
         <section className="sidebar">
             <div className="sidebar-content">
                 <Box w="100%">
-                    <List spacing={3}>
+                    <List>
                         <ListItem className="sidebar-item-hover">
-                            <Link href="/">
+                            <NextLink href="/">
                                 <a>
                                     <Icon as={AiFillHome} mr={2} boxSize="1.4rem" /> For You
                                 </a>
-                            </Link>
+                            </NextLink>
                         </ListItem>
                         <ListItem className="sidebar-item-hover">
-                            <Link href="/">
+                            <NextLink href="/">
                                 <a>
                                     <Icon as={FiUsers} mr={2} boxSize="1.4rem" /> Following
                                 </a>
-                            </Link>
+                            </NextLink>
                         </ListItem>
                     </List>
+                </Box>
+                <Box w="100%">
+                    <Text>Log in to follow creators, like videos, and view comments.</Text>
+                    <Button width="100%" variant="pink-outline" size="lg">
+                        Log in
+                    </Button>
+                </Box>
+                <Box w="100%">
+                    <Flex>
+                        <Box>
+                            <Text>Suggested accounts</Text>
+                        </Box>
+                        <Spacer />
+                        <Box>
+                            <Link>See all</Link>
+                        </Box>
+                    </Flex>
+                </Box>
+                <Box w="100%">
+                    <List spacing={5}>
+                        {[0, 1, 2, 3, 4, 5].map(i => (
+                            <ListItem>
+                                <Link>
+                                    <HStack spacing="15px">
+                                        <Box w="45px">
+                                            <Avatar size="45px" name="Dan Abrahmov" src="https://bit.ly/dan-abramov" />
+                                        </Box>
+                                        <Box>
+                                            <Text fontSize="14px" className="account__name" fontWeight="bold">
+                                                beatvn__offical
+                                            </Text>
+                                            <Text fontSize="11px" textTransform="uppercase">
+                                                BEATVN
+                                            </Text>
+                                        </Box>
+                                    </HStack>
+                                </Link>
+                            </ListItem>
+                        ))}
+                    </List>
+                </Box>
+                <Box w="100%">
+                    <Text>Discover</Text>
+                    <Wrap>
+                        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(i => (
+                            <WrapItem>
+                                <Link>
+                                    <Tag>#danhbayvirus</Tag>
+                                </Link>
+                            </WrapItem>
+                        ))}
+                    </Wrap>
                 </Box>
             </div>
         </section>
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from "react";
 import Head from "next/head";
 import DefaultLayout from "../components/layouts/DefaultLayout";
 import Sidebar from "../components/home/Sidebar";
+import { Stack, Flex, Box } from "@chakra-ui/react";
 
 class Home extends PureComponent {
     constructor(props) {
@@ -22,9 +23,13 @@ class Home extends PureComponent {
                     <link rel="icon" href="/favicon.ico" />
                 </Head>
                 <div className="page-content home-wrapper">
-                    <div className="content-left">
-                        <Sidebar />
-                    </div>
+                    <Stack direction={{ base: "column", lg: "row" }} spacing="24px">
+                        <Box w="100%" flex={{ base: "1", lg: "0 324px" }} overflow="hidden">
+                            <Sidebar />
+                        </Box>
+
+                        <Box w="100%" flex="1" overflow="hidden"></Box>
+                    </Stack>
                 </div>
             </DefaultLayout>
         );
